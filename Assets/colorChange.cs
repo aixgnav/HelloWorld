48c70092ade47d0102bbb29c7f47eb9ff896830c
aixgnav <004935823@coyote.csusb.edu>
2015-10-22 22:44:51 -0700

    HelloWorld HW

---
+++ Assets/colorChange.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colorChange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.magenta;
+        }
+
+    }
+}
